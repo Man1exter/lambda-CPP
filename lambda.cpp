4a78121a208cec8a209e0e6d393ec3667149e5b4
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-11 10:47:48 +0100

    create first lambda way

--- lambda.cpp
+++ lambda.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+auto lambda1 = []{
+cout << "Po pozniejszym wywolaniu(w main) ~ lambda" << endl;
+};
+
 int main(){
 
+    lambda1();
 
     return 0;
 }
