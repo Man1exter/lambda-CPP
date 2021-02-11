d7182d1f8af92c0f1c43d4ffd0c2d5cd25c3302f
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-11 10:54:46 +0100

    add licznik to sum show function

--- lambda.cpp
+++ lambda.cpp
@@ -8,9 +8,26 @@ auto lambda1 = []{
 cout << "Po pozniejszym wywolaniu(w main) ~ lambda" << endl;
 };
 
-int main(){
+auto lambda2 = []{
+    int wciagana_lambda = 1;
+    cout << "Ze zmienna w srodku" << wciagana_lambda << endl;
+};
 
+auto licznik = [ i = 0]() mutable { cout << "To jest " << ++i << " wywolanie licznika" << endl;};
+
+int main(){
     lambda1();
+
+    lambda2();
+
+    licznik();
+    licznik();
+    licznik();
+    licznik();
+
 
     return 0;
 }
+
+
+
